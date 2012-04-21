c41148397bb45481358b86a5d49da06099f78fed
Darren Cauthon <darren@cauthon.com>
2012-04-21 18:21:12 -0500

    Add card view.

--- app.rb
+++ app.rb
@@ -36,6 +36,12 @@ get '/dashboard' do
   haml :dashboard
 end
 
+get '/board/:board_id/card/:card_id' do
+  @board_id = params[:board_id]
+  @card = LeanKitKanban::Card.find(params[:board_id], params[:card_id])[0]
+  haml :card
+end
+
 get '/board/:id' do
   @board = LeanKitKanban::Board.find(params[:id])[0]
   @lanes = @board["Lanes"]
